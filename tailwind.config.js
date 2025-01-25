c6f06e40a2dd7c600d3edf189ebaa6f6da27c658
Vladislav Zhurbenko <vzhurbenko@vzhurbenko.dev>
2025-01-26 01:16:11 +0300

    cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import { customColors } from '.src/assets/scripts/colorLib.js'
 export default {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     content: [],
